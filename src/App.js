99f93a8e2a809c4d0abf7e706d34a2ff00361e15
Faith Kariuki <tcwamboi@gmail.com>
2024-08-06 15:23:28 +0300

    Complete lab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState, useEffect } from "react";
+import NavBar from "./components/NavBar";
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() =>{
+    fetch("http://localhost:4000/users")
+    .then(r => r.json())
+    .then(data => setUsers(data))
+    .catch(error => console.error(error));
+}, []);
   return (
     <>
       <header>
-        
+        <NavBar />
       </header>
+      {/* Signal parent component to render different components as children depending on the route a user visits */}
+      <Outlet context={users} />
     </>
   );
 };
